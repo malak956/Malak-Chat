3bd3a72a8dff7bc5154c35fb58575ab78e65ee56
malakbakhouti <malakbakhouti7@gmail.com>
2026-03-11 23:44:34 +0000

    initialize TaskFlow React project

---
+++ taskflow/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>TaskFlow</h1>
+      <p>Project Management App</p>
+    </div>
+  )
+}
+
+export default App
